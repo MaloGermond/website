c2b7fe863c4d1b28f2284ed77bf13531dc44dc30
Malo <Malo.germond@privowny.com>
2020-05-23 17:17:52 +0200

    Add description into manifest.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,11 +11,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Malo Germond`,
+        name: `Malo Germond Designer UI`,
         short_name: `Malo Germond`,
+        description: `Je suis designer
+        J’utilise sketch pour mes interfaces et illustrator pour mes illustrations.
+        Je code en Javascript pour à peu près tout (React, NodeJS) et Processing pour mes maquettes.
+        Je modélise sous Rhino pour l’industriel et sous Blender pour faire des rendus 3D.
+        J’aime dessiner avec mes crayons ;)`,
+        lang: `fr`,
         start_url: `/`,
-        background_color: `#6b37bf`,
-        theme_color: `#6b37bf`,
+        background_color: `#fff`,
+        theme_color: `#f98686`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
