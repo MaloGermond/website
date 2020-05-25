dec740a2128bed19ce599b4a4e2141be341639b9
Malo <Malo.germond@privowny.com>
2020-05-25 17:17:09 +0200

    Revert "Remove className error from footer"
    This reverts commit b2209c0060fe346b5cf95e0358964c4ea1487ce7.

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,20 +10,33 @@ const Link = styled.a`
 
 function Footer() {
   return (
-    <footer className="sticky">
+    <footer class="sticky">
       <Link
+        className={styles.link}
         href="https://github.com/MaloGermond?tab=repositories"
         target="_blank"
       >
         Github
       </Link>
-      <Link href="https://www.linkedin.com/in/malogermond/" target="_blank">
+      <Link
+        className={styles.link}
+        href="https://www.linkedin.com/in/malogermond/"
+        target="_blank"
+      >
         Linkedin
       </Link>
-      <Link href="https://twitter.com/MaloGermond" target="_blank">
+      <Link
+        className={styles.link}
+        href="https://twitter.com/MaloGermond"
+        target="_blank"
+      >
         Twitter
       </Link>
-      <Link href="http://www.malogermond.net/" target="_blank">
+      <Link
+        className={styles.link}
+        href="http://www.malogermond.net/"
+        target="_blank"
+      >
         Old website
       </Link>
     </footer>
