3b8b01c8ecd02d64228f140679cac968bda8de22
Malo <Malo.germond@gmail.com>
2020-06-22 20:03:04 +0200

    restore standart status for seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,26 +19,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Malo Germond Designer UI`,
-        short_name: `Malo Germond`,
-        description: `Je suis designer
-        J’utilise sketch pour mes interfaces et illustrator pour mes illustrations.
-        Je code en Javascript pour à peu près tout (React, NodeJS) et Processing pour mes maquettes.
-        Je modélise sous Rhino pour l’industriel et sous Blender pour faire des rendus 3D.
-        J’aime dessiner avec mes crayons ;)`,
-        lang: `FR-fr`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#f98686`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
-        icon: `src/assets/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
@@ -47,3 +27,27 @@ module.exports = {
     },
   ],
 }
+
+/*
+
+{
+  resolve: `gatsby-plugin-manifest`,
+  options: {
+    name: `Malo Germond Designer UI`,
+    short_name: `Malo Germond`,
+    description: `Je suis designer
+    J’utilise sketch pour mes interfaces et illustrator pour mes illustrations.
+    Je code en Javascript pour à peu près tout (React, NodeJS) et Processing pour mes maquettes.
+    Je modélise sous Rhino pour l’industriel et sous Blender pour faire des rendus 3D.
+    J’aime dessiner avec mes crayons ;)`,
+    lang: `FR-fr`,
+    start_url: `/`,
+    background_color: `#fff`,
+    theme_color: `#f98686`,
+    // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+    // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+    display: `standalone`,
+    icon: `src/assets/favicon.png`, // This path is relative to the root of the site.
+  },
+},
+*/
