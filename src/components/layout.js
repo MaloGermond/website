89dccc73d83ebe01e6c8258d437932fa1deb8f0d
Malo <Malo.germond@gmail.com>
2020-06-27 11:08:37 +0200

    First time SEO componenent works

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Footer from "../components/footer.js"
+import SEO from "../components/seo.js"
 
 function Layout({ children }) {
   return (
     <div>
+      <SEO />
       <div>{children}</div>
       <Footer />
     </div>
---
+++ src/components/seo.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+function SEO() {
+  return <Helmet title="homepage"></Helmet>
+}
+
+export default SEO
