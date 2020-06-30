17c05b4e9ef7e95d4f6de318470247c330bfd3a0
Malo <Malo.germond@gmail.com>
2020-06-30 19:22:21 +0200

    add descripton and keywords tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,14 +1,44 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ title, lang, pathname }) {
+function SEO({ title, description, lang, pathname }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            keywords
+            siteUrl
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+
   const canonical = pathname ? `http://malogermond.fr${pathname}` : null
+
   return (
     <Helmet
       title={title}
       htmlAttributes={{
         lang,
       }}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          name: "keywords",
+          content: site.siteMetadata.keywords,
+        },
+      ]}
       link={
         canonical
           ? [
