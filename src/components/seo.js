bfffe8f411252198621a62a5b1c4402567c46b18
Malo <Malo.germond@gmail.com>
2020-06-30 19:03:56 +0200

    Add title, lang and canonical attribute

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,8 +1,28 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-function SEO() {
-  return <Helmet title="homepage"></Helmet>
+function SEO({ title, lang, pathname }) {
+  const canonical = pathname ? `http://malogermond.fr${pathname}` : null
+  return (
+    <Helmet
+      title={title}
+      htmlAttributes={{
+        lang,
+      }}
+      link={
+        canonical
+          ? [
+              {
+                rel: "canonical",
+                href: canonical,
+              },
+            ]
+          : []
+      }
+    ></Helmet>
+  )
 }
+
+SEO.defaultProps = { title: "Malo Germond", lang: "FR-fr", pathname: "/" }
 
 export default SEO
