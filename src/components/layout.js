0e8e984c724882985e5ee19d09b4a27e82148549
Malo <Malo.germond@gmail.com>
2020-07-01 22:33:55 +0200

    Clean footer position

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,23 @@
 import React from "react"
 import Footer from "../components/footer.js"
 import SEO from "../components/seo.js"
+import styled from "styled-components"
+
+const Site = styled.div`
+  display: grid;
+`
+const Content = styled.article`
+  min-height: calc(100vh - 8.5rem);
+`
 
 function Layout({ children, pathname }) {
   return (
     <div>
       <SEO pathname={pathname} />
-      <div>{children}</div>
-      <Footer />
+      <Site>
+        <Content>{children}</Content>
+        <Footer />
+      </Site>
     </div>
   )
 }
