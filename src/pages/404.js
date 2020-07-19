bba051086a69ccc7381c6fbc5b6af353d49ed1f6
Malo <Malo.germond@privowny.com>
2020-07-19 21:52:57 +0200

    Clean 404 error

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Error = styled.article`
+  text-align: center;
+`
 
 function error() {
   return (
     <section>
-      <article>
+      <Error>
         <h3>
           Oups...{" "}
           <span role="img" aria-label="Pardon pour l'erreur">
@@ -16,7 +21,7 @@ function error() {
           Bon soit là page n'existe pas... soit il se passe un truc louche...
         </p>
         <Link>Retour à la page principale.</Link>
-      </article>
+      </Error>
     </section>
   )
 }
