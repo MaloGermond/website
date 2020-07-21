28032f6b42117f122af87d79c460cb7bbdd64ebe
Malo <Malo.germond@privowny.com>
2020-07-21 17:09:20 +0200

    hot fixe from outbound link on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,8 +18,7 @@ const Home = () => (
             href=" https://privowny.io/"
             target="_blank"
             class="header-link"
-          />
-          <OutboundLink href=" https://privowny.io/" target="_blank">
+          >
             Privowny
           </OutboundLink>
           pour apporter un moyen plus simple de protéger les données des
