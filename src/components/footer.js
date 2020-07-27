7dcd25cf2f6b7f0a18b6285a4f7426047f33763b
MaloGermond <Malo.germond@privowny.com>
2020-07-27 17:28:47 +0200

    issue 39 Refacto footer and add icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
 import React from "react"
-import "./footer.css"
 import styled from "styled-components"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 
@@ -19,17 +18,51 @@ const Icon = styled.i`
   }
 `
 
-const Colophon = styled.div``
+const SocialNetwork = styled.div`
+  display: flex;
+  padding: 1rem;
+  gap: 0.5rem;
+
+  & a {
+    border-bottom-style: none;
+  }
+`
+
+const Colophon = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Container = styled.footer`
+  background-color: var(--colors-background-light-medium);
+  padding: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  box-sizing: border-box;
+`
 
 function Footer() {
   return (
-    <footer>
+    <Container>
       <Colophon>
         <Caption>
           Copyright © 2016 - 2020 - All Rights Reserved - Marseille{" "}
         </Caption>
+        <Caption>
+          Thanks to{" "}
+          <OutboundLink href="https://www.gatsbyjs.org/" target="_blank">
+            Gatsby{" "}
+          </OutboundLink>
+          community for this awsome tool{" "}
+          <span aria-label="rocket icons" role="img">
+            🚀
+          </span>{" "}
+        </Caption>
       </Colophon>
-      <div className="externalLink">
+      <SocialNetwork>
         <OutboundLink
           href="https://github.com/MaloGermond?tab=repositories"
           target="_blank"
@@ -58,8 +91,8 @@ function Footer() {
         >
           <Icon className="icon-website"></Icon>
         </OutboundLink>
-      </div>
-    </footer>
+      </SocialNetwork>
+    </Container>
   )
 }
 
