7ef4227b3c72bdd701ed03bc0c3b20d0d21bac37
MaloGermond <malo.germond@gmail.com>
2020-11-29 11:59:52 +0100

    Add layer order and homepage value to content.md

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -8,36 +8,45 @@ const Container = styled.section`
   margin-top: 4rem;
   padding: 2rem;
   display: flex;
-  flex-wrap: nowrap;
-  justify-content: left;
+  flex-wrap: wrap;
+  justify-content: center;
   box-sizing: border-box;
   gap: 1rem;
   overflow: scroll;
+
+  @media (max-width: 768px) {
+    padding: 1rem;
+  }
  `
 
 const Cover = styled.article`
-  min-width: 256px;
-
-  .gatsby-image-wrapper {
-    transition: 0.4s;
+  width: 256px;
+  order: ${props => props.order};
+  div {
     border-radius: 4px;
+  }
+  a .gatsby-image-wrapper {
+    transition: 0.4s;
   }
 
   a:hover .gatsby-image-wrapper {
     box-shadow: var(--box-shadow-4);
+  }
+
+  @media (max-width: 768px) {
+    width: 128px;
   }
 `
 
 function Gallery() {
   const data = useStaticQuery(graphql`
     {
-      allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/(/projects/)/" } }
-      ) {
+      allMarkdownRemark(filter: { frontmatter: { homepage: { eq: true } } }) {
         nodes {
           frontmatter {
             title
             link
+            order
             cover {
               childImageSharp {
                 fluid(maxHeight: 400, quality: 100) {
@@ -54,7 +63,7 @@ function Gallery() {
   return (
     <Container>
       {data.allMarkdownRemark.nodes.map(project => (
-        <Cover>
+        <Cover order={project.frontmatter.order}>
           <Link
             to={project.frontmatter.link}
             target="_blank"
